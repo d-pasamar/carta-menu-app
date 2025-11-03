8ea247ae445427d1591c7744ff7a4337e7efece1
David <david.pasamar@gmail.com>
2025-11-03 12:05:25 +0000

    Update: Se separan datos en fichero menuData.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,11 @@ import Header from "./components/Header/Header";
 import Menu from "./components/Menu/Menu";
 import Footer from "./components/Footer/Footer";
 import Line from "./components/Line";
-import "./App.css";
 
-const menuData = [
-  {
-    title: "Coffee",
-    image: "https://cdn.freecodecamp.org/curriculum/css-cafe/coffee.jpg",
-    items: [
-      { name: "French Vanilla", price: "3.00" },
-      { name: "Caramel Macchiato", price: "3.75" },
-      { name: "Pumpkin Spice", price: "3.50" },
-      { name: "Hazelnut", price: "4.00" },
-      { name: "Mocha", price: "4.50" },
-    ],
-  },
-  {
-    title: "Desserts",
-    image: "https://cdn.freecodecamp.org/curriculum/css-cafe/pie.jpg",
-    items: [
-      { name: "Donut", price: "1.50" },
-      { name: "Cherry Pie", price: "2.75" },
-      { name: "Cheesecake", price: "3.00" },
-      { name: "Cinnamon Roll", price: "2.50" },
-    ],
-  },
-];
+// DATOS BASE DE LA APP
+import { menuData } from "./data/menuData";
+
+import "./App.css";
 
 function App() {
   return (
---
+++ src/data/menuData.js
@@ -0,0 +1,31 @@
+// src/data/menuData.js
+// Este archivo contiene los datos estáticos del menú del restaurante.
+// Comprende de un array de objetos.
+// Cada sección incluye:
+// - 'title': nombre de la categoría
+// - 'image': una URL con el icono representativo de la categoría
+// - 'items': array de objetos con 'name' y 'price' para cada producto
+
+export const menuData = [
+  {
+    title: "Coffee",
+    image: "https://cdn.freecodecamp.org/curriculum/css-cafe/coffee.jpg",
+    items: [
+      { name: "French Vanilla", price: "3.00" },
+      { name: "Caramel Macchiato", price: "3.75" },
+      { name: "Pumpkin Spice", price: "3.50" },
+      { name: "Hazelnut", price: "4.00" },
+      { name: "Mocha", price: "4.50" },
+    ],
+  },
+  {
+    title: "Desserts",
+    image: "https://cdn.freecodecamp.org/curriculum/css-cafe/pie.jpg",
+    items: [
+      { name: "Donut", price: "1.50" },
+      { name: "Cherry Pie", price: "2.75" },
+      { name: "Cheesecake", price: "3.00" },
+      { name: "Cinnamon Roll", price: "2.50" },
+    ],
+  },
+];
